205ec053059f1adc205db50a2358e5a0ac11d11a
MGcommits <marthagamez211@gmail.com>
2021-03-09 15:34:44 -0600

    Create tables and queries

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary decimal NOT NULL,  1
+  department_id INTEGER,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id integer,
+  manager_id integer,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO department (name)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
+  ('Legal');
+  
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Sales Lead', 100000, 1),
+  ('Salesperson', 80000, 1),
+  ('Lead Engineer', 150000, 2),
+  ('Software Enginner', 120000, 2),
+  ('Accountant', 125000, 3),
+  ('Accountant Assistant', 90000, 3),
+  ('Legal Team Lead', 250000, 4),
+  ('Lawyer', 190000, 4);
+  
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, NULL),
+  ('Virginia', 'Woolf', 2, 1),
+  ('Piers', 'Gaveston', 3, NULL),
+  ('Charles', 'LeRoi', 4, 3),
+  ('Katherine', 'Mansfield', 5, NULL),
+  ('Dora', 'Carrington', 6, 5),
+  ('Edward', 'Bellamy', 7, NULL),
+  ('Montague', 'Summers', 8, 7),
+  ('Octavia', 'Butler', 4, 3),
+  ('Unica', 'Zurn', 2, 1);
+  
