0319c6eb2ec85b249bd2a23e5678275ad3f9b16a
MGcommits <marthagamez211@gmail.com>
2021-03-10 12:02:29 -0600

    Ad and display  role and all employees options

--- app.js
+++ app.js
@@ -28,28 +28,20 @@ function Prompt(){
         }])
         .then(({options}) => {
             if(options == 'View all departments'){
-                // Call function to handle this option
+                // Call function to handle this All departments option
                 viewAllDepartments();
             } else
             if (options == 'View all roles'){
-                //Call fucntion to handle this option
+                //Call function to handle this View all roles option
                 viewAllRoles();
+            } else
+            if(options == 'View all employees'){
+                //Call function to handle this View all roles View all employees option
+                wiewAllEmployees();
             }
         });
 }
-/*
-// call once somewhere in the beginning of the app
-const cTable = require('console.table');
-console.table([
-  {
-    name: 'foo',
-    age: 10
-  }, {
-    name: 'bar',
-    age: 20
-  }
-]);
-*/
+
 function viewAllDepartments(){
     // Use query to bring the list of all departments
     connection.query("SELECT * FROM department",
@@ -66,16 +58,25 @@ function viewAllDepartments(){
 
 function viewAllRoles(){
     // Use query to bring the list of all departments
-    connection.query("SELECT * FROM department",
-    function(err, results, fields) {
-        var arrToDisplay = [];
-        //console.log(results);
-        for(var i=0; i<results.length; i++){
-            arrToDisplay.push(results[i].name);
-        }
-        console.table(results); // results contains rows returned by server
-        //console.log(fields); // fields contains extra meta data about results, if available
-      }); 
+    connection.query("SELECT role.title, role.id, department.name, role.salary FROM role INNER JOIN department ON role.department_id = department.id",
+    function(err, results, fields){
+    
+        console.table(results); 
+
+    }); 
+}
+
+function wiewAllEmployees(){
+    var strQuery = "SELECT e.id, e.first_name, e.last_name, role.title AS job_title, department.name AS departmentes, role.salary AS salaries, Concat(m.first_name,' ', m.last_name) AS manager " +
+        "FROM employee e " +
+        "LEFT JOIN employee m ON e.manager_id = m.id " +
+        "LEFT JOIN role ON e.role_id=role.id " +
+        "LEFT JOIN department ON role.department_id=department.id";
+    connection.query(strQuery,
+    function(err, results, fields){
+        console.table(results);
+
+    });
 }
 
 Prompt();
